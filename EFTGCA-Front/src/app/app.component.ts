eabfc291958436965a0c81239b110dded833b1cf
Miguel Soriano Carceller <mscarceller@gmail.com>
2020-09-29 16:48:19 +0200

    Change app title

--- EFTGCA-Front/src/app/app.component.ts
+++ EFTGCA-Front/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ElasticFaultTolerant-GroupChat';
+  title = 'Elastic & FaultTolerant GroupChat Application';
 }
