d756b207155997b698439d97696b380bd824a9bf
Miguel Soriano Carceller <mscarceller@gmail.com>
2020-10-07 13:36:03 +0200

    Add color to user name in chat messages

--- EFTGCA-Front/src/app/chatmessage/chatmessage.component.ts
+++ EFTGCA-Front/src/app/chatmessage/chatmessage.component.ts
@@ -31,11 +31,26 @@ export class ChatmessageComponent implements OnInit {
       return "#ffffff";
   }
 
+  getUserNameColor(){
+    return this.hashStringToColor(this.userName)
+  }
+
   isSent(){
     if (this.type=="sent")
       return this.ack;
     else
       return null;
+  }
+
+   hashStringToColor(str) {
+    var hash = 0;
+    for (var i = 0; i < str.length; i++) {
+      hash = ((hash << 5) + hash) + str.charCodeAt(i); /* hash * 33 + c */
+    }
+    var r = (hash & 0xFF0000) >> 16;
+    var g = (hash & 0x00FF00) >> 8;
+    var b = hash & 0x0000FF;
+    return "#" + ("0" + r.toString(16)).substr(-2) + ("0" + g.toString(16)).substr(-2) + ("0" + b.toString(16)).substr(-2);
   }
 
 
