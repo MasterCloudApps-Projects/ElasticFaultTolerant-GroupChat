f79d75a118bdca2a769b68b12e6838ea2368b641
mscarceller <mscarceller@gmail.com>
2020-10-28 18:06:57 +0100

    Fix API Rest url

--- EFTGCA-Front/src/environments/environment.prod.ts
+++ EFTGCA-Front/src/environments/environment.prod.ts
@@ -2,6 +2,7 @@ export const environment = {
   production: true,
 
   CHAT_URL : "wss://webchat-mscarceller.cloud.okteto.net/chat",
-  CHAT_IMAGES_URL: "https://webchat-mscarceller.cloud.okteto.net"
+  CHAT_IMAGES_URL: "https://webchat-mscarceller.cloud.okteto.net",
+  CHAT_FILES_URL: "https://webchat-mscarceller.cloud.okteto.net"
   
 };
